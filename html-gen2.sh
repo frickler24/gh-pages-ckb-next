39cb5c1dc63eb7325937df4873e6df1bbb5471a6
Lutz Eichler <github@frickler24.de>
2017-04-24 23:27:28 +0200

    Implement Scripts to patch HTML entry files

---
+++ html-gen2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "x$1" == "x." ] ; then echo "found ., ignored" ; exit 0; fi
+if [ "x$1" == "x./.git" ] ; then echo "found .git, ignored" ; exit 0; fi
+if [ "x$1" == "x./bin" ] ; then echo "found bin, ignored" ; exit 0; fi
+
+DIRNAME=$(echo $1 | cut -d/ -f2)
+echo "proceeding with ${DIRNAME}"
+sed s/BRANCH/$DIRNAME/g ../gh-pages-ckb-next/snippet-file >> content
