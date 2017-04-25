dfa3e9a17b9c313771667b2d9f482e5b9361c448
Lutz Eichler <github@frickler24.de>
2017-04-25 21:55:28 +0200

    Fix regexp in sed and reduce debug output

--- html-gen2.sh
+++ html-gen2.sh
@@ -5,13 +5,13 @@
 ####
 
 # Check which dirnames are unwanted
-if [ "x$1" == "x." ] ; then echo "found ., ignored" ; exit 0; fi
-if [ "x$1" == "x./.git" ] ; then echo "found .git, ignored" ; exit 0; fi
-if [ "x$1" == "x./bin" ] ; then echo "found bin, ignored" ; exit 0; fi
+if [ "x$1" == "x." ] ; then exit 0; fi
+if [ "x$1" == "x./.git" ] ; then exit 0; fi
+if [ "x$1" == "x./bin" ] ; then exit 0; fi
 
 # dirnames are in ./directoy but we need directory only
 DIRNAME=$(echo $1 | cut -d/ -f2)
-echo "proceeding with ${DIRNAME}"
+echo "Generating link for ${DIRNAME}"
 
 # Assume that content file was cleared before the first call.
 sed s/BRANCH/$DIRNAME/g snippet-file >> content
--- html-generator.sh
+++ html-generator.sh
@@ -11,11 +11,11 @@ rm -f content{,html,pdf}
 # Filtering for unwanted dirs occurs in the script called
 find . -maxdepth 1 -type d -exec html-gen2.sh {} \;
 
-# Now path the result for html and pdf links
+# echo Now change the result for html and pdf links
 sed s/FORMATDEFINITION/html/g content > contenthtml
-sed s/FORMATDEFINITION/latex\/refman.pdf/g content > contentpdf
+sed s\!FORMATDEFINITION\!latex/refman.pdf\!g content > contentpdf
 
-# replace the searchstring with the new contents
+# echo replace the searchstring with the new content
 awk 'NR==FNR { a[n++]=$0; next }
 /PUT_SECTION_HERE/ { for (i=0;i<n;++i) print a[i]; next }
 1' contenthtml pres-html.html > /tmp/xx && mv /tmp/xx pres-html.html
